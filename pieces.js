0883ad90b32f565fdc4329c50581d4b3fb63b65b
boby-front <delanglebob38@gmail.com>
2023-08-08 18:43:19 +0200

    add contents and pictures

--- pieces.js
+++ pieces.js
@@ -24,9 +24,38 @@ function createCardAndRedirect(type) {
       const card = document.createElement("div");
       card.classList.add("card");
 
-      const image = document.createElement("img");
-      image.src = item.image;
-      card.appendChild(image);
+      if (Array.isArray(item.image) && item.image.length > 1) {
+        const carousel = document.createElement("div");
+        carousel.classList.add("carousel");
+
+        const carouselImages = document.createElement("div");
+        carouselImages.classList.add("carousel-images");
+        const carouselBullets = document.createElement("div");
+        carouselBullets.classList.add("carousel-bullets");
+
+        item.image.forEach((imgSrc, index) => {
+          const image = document.createElement("img");
+          image.src = imgSrc;
+          carouselImages.appendChild(image);
+
+          // Création des bullet points
+          const bullet = document.createElement("span");
+          bullet.classList.add("carousel-bullet");
+          if (index === 0) {
+            bullet.classList.add("active");
+          }
+          carouselBullets.appendChild(bullet);
+        });
+
+        carousel.appendChild(carouselImages);
+        carousel.appendChild(carouselBullets);
+
+        card.appendChild(carousel);
+      } else {
+        const image = document.createElement("img");
+        image.src = Array.isArray(item.image) ? item.image[0] : item.image;
+        card.appendChild(image);
+      }
 
       const title = document.createElement("h2");
       title.textContent = item.titre;
@@ -46,3 +75,30 @@ function createCardAndRedirect(type) {
     });
   }
 }
+
+//Caroussel gestion du click
+
+document.addEventListener("click", function (event) {
+  if (event.target.closest(".carousel")) {
+    const carousel = event.target.closest(".carousel");
+    const currentImage = carousel.querySelector(
+      'img:not([style*="display: none"])'
+    );
+    const nextImage =
+      currentImage.nextElementSibling ||
+      carousel.querySelector("img:first-child");
+
+    // Gestion des bullet points
+    const bullets = carousel.querySelectorAll(".carousel-bullet");
+    let currentBulletIndex = [...carousel.querySelectorAll("img")].indexOf(
+      currentImage
+    );
+    bullets[currentBulletIndex].classList.remove("active");
+
+    currentBulletIndex = (currentBulletIndex + 1) % bullets.length;
+    bullets[currentBulletIndex].classList.add("active");
+
+    currentImage.style.display = "none";
+    nextImage.style.display = "block";
+  }
+});
